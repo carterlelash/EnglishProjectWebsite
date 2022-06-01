78b8a9223dc0de4a002d3db2c4120df80647f7d2
carterlelash <lelashcarter@gmail.com>
2022-06-01 12:44:15 -0700

    Functionality and positioning changes

--- index.js
+++ index.js
@@ -29,64 +29,128 @@ let caption1960Four = document.getElementById("apartheidImageCaption4");
 let image1980Five = document.getElementById("apartheidImage5");
 let caption1980Five = document.getElementById("apartheidImageCaption5");
 
+let image1980Six = document.getElementById("apartheidImage6");
+let caption1980Six = document.getElementById("apartheidImageCaption6");
 function revealText (event) {
     if (event.target == button1948) {
-        text1948.style.visibility = "visible";
-        text1950.style.visibility = "hidden";
-        text1960.style.visibility = "hidden";
-        text1970.style.visibility = "hidden";
-        text1980.style.visibility = "hidden";
-        text1990.style.visibility = "hidden";
+    text1948.style.visibility = "visible";
+    text1950.style.visibility = "hidden";
+    text1960.style.visibility = "hidden";
+    text1970.style.visibility = "hidden";
+    text1980.style.visibility = "hidden";
+    text1990.style.visibility = "hidden";
+    image1990One.style.visibility = "hidden";
+    caption1990One.style.visibility = "hidden";
+    image1990Two.style.visibility = "hidden";
+    caption1990Two.style.visibility = "hidden";
+    image1960Three.style.visibility = "hidden";
+    caption1960Three.style.visibility = "hidden";
+    image1960Four.style.visibility = "hidden";
+    caption1960Four.style.visibility = "hidden";
+    image1980Five.style.visibility = "hidden";
+    caption1980Five.style.visibility = "hidden";
+    image1980Six.style.visibility = "hidden";
+    caption1980Six.style.visibility = "hidden";
     }
     if (event.target == button1950) {
-        text1948.style.visibility = "hidden";
-        text1950.style.visibility = "visible";
-        text1960.style.visibility = "hidden";
-        text1970.style.visibility = "hidden";
-        text1980.style.visibility = "hidden";
-        text1990.style.visibility = "hidden";
-        image1980Five.style.visibility = "visible";
-        caption1980Five.style.visibility = "visible";
+    text1948.style.visibility = "hidden";
+    text1950.style.visibility = "visible";
+    text1960.style.visibility = "hidden";
+    text1970.style.visibility = "hidden";
+    text1980.style.visibility = "hidden";
+    text1990.style.visibility = "hidden";
+    image1990One.style.visibility = "hidden";
+    caption1990One.style.visibility = "hidden";
+    image1990Two.style.visibility = "hidden";
+    caption1990Two.style.visibility = "hidden";
+    image1960Three.style.visibility = "hidden";
+    caption1960Three.style.visibility = "hidden";
+    image1960Four.style.visibility = "hidden";
+    caption1960Four.style.visibility = "hidden";
+    image1980Five.style.visibility = "hidden";
+    caption1980Five.style.visibility = "hidden";
+    image1980Six.style.visibility = "hidden";
+    caption1980Six.style.visibility = "hidden";
     }
     if (event.target == button1960) {
-        text1948.style.visibility = "hidden";
-        text1950.style.visibility = "hidden";
-        text1960.style.visibility = "visible";
-        text1970.style.visibility = "hidden";
-        text1980.style.visibility = "hidden";
-        text1990.style.visibility = "hidden";
-        image1960Three.style.visibility = "visible";
-        caption1960Three.style.visibility = "visible";
-        image1960Four.style.visibility = "visible";
-        caption1960Four.style.visibility = "visible";
+    text1948.style.visibility = "hidden";
+    text1950.style.visibility = "hidden";
+    text1960.style.visibility = "visible";
+    text1970.style.visibility = "hidden";
+    text1980.style.visibility = "hidden";
+    text1990.style.visibility = "hidden";
+    image1990One.style.visibility = "hidden";
+    caption1990One.style.visibility = "hidden";
+    image1990Two.style.visibility = "hidden";
+    caption1990Two.style.visibility = "hidden";
+    image1960Three.style.visibility = "visible";
+    caption1960Three.style.visibility = "visible";
+    image1960Four.style.visibility = "visible";
+    caption1960Four.style.visibility = "visible";
+    image1980Five.style.visibility = "hidden";
+    caption1980Five.style.visibility = "hidden";
+    image1980Six.style.visibility = "hidden";
+    caption1980Six.style.visibility = "hidden";
     }
     if (event.target == button1970) {
-        text1948.style.visibility = "hidden";
-        text1950.style.visibility = "hidden";
-        text1960.style.visibility = "hidden";
-        text1970.style.visibility = "visible";
-        text1980.style.visibility = "hidden";
-        text1990.style.visibility = "hidden";
+    text1948.style.visibility = "hidden";
+    text1950.style.visibility = "hidden";
+    text1960.style.visibility = "hidden";
+    text1970.style.visibility = "visible";
+    text1980.style.visibility = "hidden";
+    text1990.style.visibility = "hidden";
+    image1990One.style.visibility = "hidden";
+    caption1990One.style.visibility = "hidden";
+    image1990Two.style.visibility = "hidden";
+    caption1990Two.style.visibility = "hidden";
+    image1960Three.style.visibility = "hidden";
+    caption1960Three.style.visibility = "hidden";
+    image1960Four.style.visibility = "hidden";
+    caption1960Four.style.visibility = "hidden";
+    image1980Five.style.visibility = "hidden";
+    caption1980Five.style.visibility = "hidden";
+    image1980Six.style.visibility = "hidden";
+    caption1980Six.style.visibility = "hidden";
     }
     if (event.target == button1980) {
-        text1948.style.visibility = "hidden";
-        text1950.style.visibility = "hidden";
-        text1960.style.visibility = "hidden";
-        text1970.style.visibility = "hidden";
-        text1980.style.visibility = "visible";
-        text1990.style.visibility = "hidden";
+    text1948.style.visibility = "hidden";
+    text1950.style.visibility = "hidden";
+    text1960.style.visibility = "hidden";
+    text1970.style.visibility = "hidden";
+    text1980.style.visibility = "visible";
+    text1990.style.visibility = "hidden";
+    image1990One.style.visibility = "hidden";
+    caption1990One.style.visibility = "hidden";
+    image1990Two.style.visibility = "hidden";
+    caption1990Two.style.visibility = "hidden";
+    image1960Three.style.visibility = "hidden";
+    caption1960Three.style.visibility = "hidden";
+    image1960Four.style.visibility = "hidden";
+    caption1960Four.style.visibility = "hidden";
+    image1980Five.style.visibility = "visible";
+    caption1980Five.style.visibility = "visible";
+    image1980Six.style.visibility = "visible";
+    caption1980Six.style.visibility = "visible";
     }
     if (event.target == button1990) {
-        text1948.style.visibility = "hidden";
-        text1950.style.visibility = "hidden";
-        text1960.style.visibility = "hidden";
-        text1970.style.visibility = "hidden";
-        text1980.style.visibility = "hidden";
-        text1990.style.visibility = "visible";
-        image1990One.style.visibility = "visible";
-        caption1990One.style.visibility = "visible";
-        image1990Two.style.visibility = "visible";
-        caption1990Two.style.visibility = "visible";
+    text1948.style.visibility = "hidden";
+    text1950.style.visibility = "hidden";
+    text1960.style.visibility = "hidden";
+    text1970.style.visibility = "hidden";
+    text1980.style.visibility = "hidden";
+    text1990.style.visibility = "visible";
+    image1990One.style.visibility = "visible";
+    caption1990One.style.visibility = "visible";
+    image1990Two.style.visibility = "visible";
+    caption1990Two.style.visibility = "visible";
+    image1960Three.style.visibility = "hidden";
+    caption1960Three.style.visibility = "hidden";
+    image1960Four.style.visibility = "hidden";
+    caption1960Four.style.visibility = "hidden";
+    image1980Five.style.visibility = "hidden";
+    caption1980Five.style.visibility = "hidden";
+    image1980Six.style.visibility = "hidden";
+    caption1980Six.style.visibility = "hidden";
     }
 }
 
@@ -107,6 +171,8 @@ function clearAll () {
     caption1960Four.style.visibility = "hidden";
     image1980Five.style.visibility = "hidden";
     caption1980Five.style.visibility = "hidden";
+    image1980Six.style.visibility = "hidden";
+    caption1980Six.style.visibility = "hidden";
 }
 
 button1948.addEventListener("click",revealText);
